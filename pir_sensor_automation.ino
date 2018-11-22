a31bff8c8acf7ac3b52004df35cf3fca68f03b41
faisalraza1011 <45238935+faisalraza1011@users.noreply.github.com>
2018-11-22 12:55:56 +0530

    Add files via upload

---
+++ pir_sensor_automation.ino
@@ -0,0 +1,19 @@
+int x,y;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(A0,INPUT);
+pinMode(3,OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+x=analogRead(A0);
+Serial.println(x);
+if(x>200){
+  digitalWrite(3,HIGH);
+}else{
+  digitalWrite(3,LOW);
+}
+}
